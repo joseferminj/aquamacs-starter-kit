2f6eb92d1f1bf4d6387c2637735b3ada1e035c17
Basil Shubin <basil.shubin@gmail.com>
2010-08-28 17:06:10 +0700

    Add new useful function: python-insert-translation

--- starter-kit-python.el
+++ starter-kit-python.el
@@ -53,6 +53,24 @@
        (add-hook 'python-mode-hook 'flymake-find-file-hook)
        )
 
+     ;; Select text block and press \C-c\C-t and voila!
+     (defun python-insert-translation (from to &optional buffer)
+       (interactive "*r")
+       (save-excursion
+         (save-restriction
+           (narrow-to-region from to)
+           (goto-char from)
+           (iso-iso2sgml from to)
+           (insert "_(")
+           (goto-char (point-max))
+           (insert ")")
+           (point-max))))
+
+     (add-hook 'python-mode-hook
+               '(lambda ()
+                  (local-set-key "\C-c\C-t" 'python-insert-translation)
+                  ))
+
      (setq-default indent-tabs-mode nil)
      (setq-default tab-width 4)
      (setq-default py-indent-offset 4)
