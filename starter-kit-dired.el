4b201757063e1b480d92242e8e6a7526f385a6cb
Basil Shubin <basil.shubin@gmail.com>
2010-08-28 13:30:13 +0700

    Customize dired mode

---
+++ starter-kit-dired.el
@@ -0,0 +1,19 @@
+;;; starter-kit-dired.el --- Making the defaults a bit saner
+;;
+;; Part of the Emacs Starter Kit
+
+(require 'dired-x)
+;; Omit unwanted files in dired-mode
+(setq dired-omit-files 
+      (rx (or (seq bol (? ".") "#") ;; emacs autosave files
+              (seq "~" eol)         ;; backup-files 
+              (seq bol "." (not (any "."))) ;; dot-files 
+              (seq ".elc" eol)      ;; compiled elisp source code
+              (seq ".pyc" eol)      ;; compiled python source code
+              ))) 
+ 
+(add-hook 'dired-mode-hook (lambda () (dired-omit-mode 1))) 
+(put 'dired-find-alternate-file 'disabled nil)
+
+(provide 'starter-kit-dired)
+;;; starter-kit-dired.el ends here
