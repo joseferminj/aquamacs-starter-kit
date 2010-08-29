47f4d61972ca180e092a092a4c2387cee6d378bd
Basil Shubin <basil.shubin@gmail.com>
2010-08-29 12:43:34 +0700

    Small fixes

--- starter-kit-yasnippet.el
+++ starter-kit-yasnippet.el
@@ -3,6 +3,7 @@
 ;; Part of the Emacs Starter Kit
 
 (yas/load-directory (expand-file-name "snippets" dotfiles-dir))
+(setq yas/use-menu 'abbreviate)
 
 (provide 'starter-kit-yasnippet)
 ;; starter-kit-yasnippet.el ends here
